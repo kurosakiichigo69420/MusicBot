151e2d6a5ce9bd830557e3c65e29957e038aa6e8
kurosakiichigo69420 <78868062+kurosakiichigo69420@users.noreply.github.com>
2022-03-22 14:19:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-        TOKEN: '', //write your discord bot token
+        TOKEN: 'OTU1Nzg5MDA3NzM5MDU2MTY4.Yjmx4Q.s8zeLyCgMwbKGaf98w8nH_8a4qU', //write your discord bot token
         px: '!',
         playing: 'Umut Bayraktar ❤️ Music Bot',
 
@@ -9,7 +9,7 @@ module.exports = {
             roleName: 'DJ', //WRITE WHAT THE NAME OF THE DJ ROLE WILL BE, THEY CAN USE IT ON YOUR SERVER
             commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //Please don't touch
         },
-        selfDeaf: false, //IF YOU WANT TO DEAF THE BOT, set false to true.
+        selfDeaf: true, //IF YOU WANT TO DEAF THE BOT, set false to true.
         maxVol: 100, //You can specify the maximum volume level.
         loopMessage: false, //Please don't touch
         discordPlayer: {
